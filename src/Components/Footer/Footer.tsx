c82c1cb084be2982fc8daddc12a25c859ca7f471
SolisTenebrum <solistenebrum@icloud.com>
2024-08-07 17:05:09 +0500

    improved footer component

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,6 @@
 import styles from './Footer.module.css';
-import twitterIcon from '../../assets/logos/logo-twitter.svg';
-import facebookIcon from '../../assets/logos/logo-fb.svg';
-import instagramIcon from '../../assets/logos/logo-instagram.svg';
-import githubIcon from '../../assets/logos/logo-github.svg';
 import mainLogo from '../../assets/logos/vrnas-full-logo.svg';
-import locationIcon from '../../assets/icons/location-icon.svg';
-import messageIcon from '../../assets/icons/message-icon.svg';
-import callIcon from '../../assets/icons/call-icon.svg';
+import { quickLinks, supportLinks, contactLinks, socialLogos } from '../../constants';
 
 const Footer = () => {
   return (
@@ -14,60 +8,51 @@ const Footer = () => {
       <div className={styles.container}>
         <div className={styles.top}>
           <div className={styles.logosContainer}>
-            <img src={mainLogo} className={styles.mainLogo} onClick={() => {
-              window.scrollTo({top: 0, behavior: 'smooth'});
-            }}/>
+            <img
+              src={mainLogo}
+              className={styles.mainLogo}
+              onClick={() => {
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }}
+            />
             <div className={styles.socialLogos}>
-              <div className={styles.logoBackground}>
-                <img src={twitterIcon} className={styles.socialLogo} />
-              </div>
-              <div className={styles.logoBackground}>
-                <img src={facebookIcon} className={styles.socialLogo} />
-              </div>
-              <div className={styles.logoBackground}>
-                <img src={instagramIcon} className={styles.socialLogo} />
-              </div>
-              <div className={styles.logoBackground}>
-                <img src={githubIcon} className={styles.socialLogo} />
-              </div>
+              {socialLogos.map((item, index) => (
+                <div className={styles.logoBackground} key={index}>
+                  <img src={item.icon} className={styles.socialLogo} />
+                </div>
+              ))}
             </div>
           </div>
           <div className={styles.linksContainer}>
             <div className={styles.linksColumn}>
               <p className={styles.linksTitle}>Quicklinks</p>
               <ul className={styles.linksList}>
-                <li className={styles.link}>Home</li>
-                <li className={styles.link}>Pricing plan</li>
-                <li className={styles.link}>Service</li>
-                <li className={styles.link}>Blog</li>
-                <li className={styles.link}>Our Team</li>
+                {quickLinks.map((link, index) => (
+                  <li key={index} className={styles.link}>
+                    {link.linkText}
+                  </li>
+                ))}
               </ul>
             </div>
             <div className={styles.linksColumn}>
               <p className={styles.linksTitle}>Support</p>
               <ul className={styles.linksList}>
-                <li className={styles.link}>About us</li>
-                <li className={styles.link}>Contact us</li>
-                <li className={styles.link}>FAQ</li>
-                <li className={styles.link}>Tems & Conditions</li>
-                <li className={styles.link}>Privacy Policy</li>
+                {supportLinks.map((link, index) => (
+                  <li key={index} className={styles.link}>
+                    {link.linkText}
+                  </li>
+                ))}
               </ul>
             </div>
             <div className={styles.linksColumn}>
               <p className={styles.linksTitle}>Need Help?</p>
               <ul className={styles.linksList}>
-                <li className={styles.link}>
-                  <img src={locationIcon} className={styles.linkIcon}/>
-                  Tanjung Sari Street no.48, Pontianak City
-                </li>
-                <li className={styles.link}>
-                  <img src={messageIcon} className={styles.linkIcon}/>
-                  Support@VRNas.com
-                </li>
-                <li className={styles.link}>
-                  <img src={callIcon} className={styles.linkIcon}/>
-                  +123 456 7890
-                </li>
+                {contactLinks.map((link, index) => (
+                  <li className={styles.link} key={index}>
+                    <img src={link.icon} className={styles.linkIcon} />
+                    {link.linkText}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
--- src/constants.ts
+++ src/constants.ts
@@ -6,46 +6,143 @@ import serviceIconFour from './assets/icons/7.svg';
 import serviceIconFive from './assets/icons/8.svg';
 import serviceIconSix from './assets/icons/9.svg';
 
-const serviceCards = [
-    {
-      icon: serviceIconOne,
-      title: 'VR Development',
-      text: `From concept to creation, our team of VR developers will bring your vision to life.`,
-      button: 'LEARN MORE',
-    },
-    {
-      icon: serviceIconTwo,
-      title: 'VR Design',
-      text: `Our talented VR designers will create immersive and engaging environments that will captivate your
-       audience.`,
-      button: 'LEARN MORE',
-    },
-    {
-      icon: serviceIconThree,
-      title: 'VR Consulting',
-      text: `Our VR consultants will work with you to ensure that your VR experience meets your goals and exceeds
-       your expectations.`,
-      button: 'LEARN MORE',
-    },
-    {
-      icon: serviceIconFour,
-      title: 'VR Games',
-      text: `We offer a wide selection of VR games that are suitable for players of all ages and skill levels.`,
-      button: 'LEARN MORE',
-    },
-    {
-      icon: serviceIconFive,
-      title: 'VR Events',
-      text: `Make your next event unforgettable with our VR event services.`,
-      button: 'LEARN MORE',
-    },
-    {
-      icon: serviceIconSix,
-      title: 'VR Entertainment',
-      text: `Create a VR escape room, or offer VR experiences at a theme park, we have the expertise and experience
-       to make it happen.`,
-      button: 'LEARN MORE',
-    },
-  ];
+// Footer contact icons
+import locationIcon from './assets/icons/location-icon.svg';
+import messageIcon from './assets/icons/message-icon.svg';
+import callIcon from './assets/icons/call-icon.svg';
 
-  export {serviceCards}
+// Footer social icons
+import twitterIcon from './assets/logos/logo-twitter.svg';
+import facebookIcon from './assets/logos/logo-fb.svg';
+import instagramIcon from './assets/logos/logo-instagram.svg';
+import githubIcon from './assets/logos/logo-github.svg';
+
+// Service section cards
+const serviceCards = [
+  {
+    icon: serviceIconOne,
+    title: 'VR Development',
+    text: `From concept to creation, our team of VR developers will bring your vision to life.`,
+    button: 'LEARN MORE',
+  },
+  {
+    icon: serviceIconTwo,
+    title: 'VR Design',
+    text: `Our talented VR designers will create immersive and engaging environments that will captivate your
+       audience.`,
+    button: 'LEARN MORE',
+  },
+  {
+    icon: serviceIconThree,
+    title: 'VR Consulting',
+    text: `Our VR consultants will work with you to ensure that your VR experience meets your goals and exceeds
+       your expectations.`,
+    button: 'LEARN MORE',
+  },
+  {
+    icon: serviceIconFour,
+    title: 'VR Games',
+    text: `We offer a wide selection of VR games that are suitable for players of all ages and skill levels.`,
+    button: 'LEARN MORE',
+  },
+  {
+    icon: serviceIconFive,
+    title: 'VR Events',
+    text: `Make your next event unforgettable with our VR event services.`,
+    button: 'LEARN MORE',
+  },
+  {
+    icon: serviceIconSix,
+    title: 'VR Entertainment',
+    text: `Create a VR escape room, or offer VR experiences at a theme park, we have the expertise and experience
+       to make it happen.`,
+    button: 'LEARN MORE',
+  },
+];
+
+// Footer social icons
+const socialLogos = [
+  {
+    icon: twitterIcon,
+    link: '/',
+  },
+  {
+    icon: facebookIcon,
+    link: '/',
+  },
+  {
+    icon: instagramIcon,
+    link: '/',
+  },
+  {
+    icon: githubIcon,
+    link: '/',
+  },
+];
+
+// Footer links
+const quickLinks = [
+  {
+    linkText: 'Home',
+    link: '/',
+  },
+  {
+    linkText: 'Pricing plan',
+    link: '/',
+  },
+  {
+    linkText: 'Service',
+    link: '/',
+  },
+  {
+    linkText: 'Blog',
+    link: '/',
+  },
+  {
+    linkText: 'Our team',
+    link: '/',
+  },
+];
+
+const supportLinks = [
+  {
+    linkText: 'About us',
+    link: '/',
+  },
+  {
+    linkText: 'Contact us',
+    link: '/',
+  },
+  {
+    linkText: 'FAQ',
+    link: '/',
+  },
+  {
+    linkText: 'Terms & Conditions',
+    link: '/',
+  },
+  {
+    linkText: 'Privacy Policy',
+    link: '/',
+  },
+];
+
+const contactLinks = [
+  {
+    linkText: 'Tanjung Sari Street no.48, Pontianak City',
+    link: '/',
+    icon: locationIcon,
+  },
+  {
+    linkText: 'Support@VRNas.com',
+    link: '/',
+    icon: messageIcon,
+  },
+  {
+    linkText: '+123 456 7890',
+    link: '/',
+    icon: callIcon,
+  },
+];
+
+export { serviceCards, quickLinks, supportLinks, contactLinks, socialLogos };
