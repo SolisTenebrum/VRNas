2930f08c290da505ffb3dd23c238fd29eb362d66
SolisTenebrum <solistenebrum@icloud.com>
2024-08-13 19:11:11 +0500

    added filter to icons in footer

--- src/Components/Homepage/Footer/Footer.tsx
+++ src/Components/Homepage/Footer/Footer.tsx
@@ -7,6 +7,7 @@ const Footer = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
