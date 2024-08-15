3e443f9ab9004bac95dc111cef41d968a76daec2
SolisTenebrum <solistenebrum@icloud.com>
2024-08-15 17:22:40 +0500

    added scroll to top

--- src/Components/DetailService/DetailService.tsx
+++ src/Components/DetailService/DetailService.tsx
@@ -2,15 +2,17 @@ import PageTitle from '../AboutUs/PageTitle/PageTitle';
 import GetStarted from '../Homepage/GetStarted/GetStarted';
 import Pricing from '../Homepage/Pricing/Pricing';
 import Subscribe from '../Homepage/Subscribe/Subscribe';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 import VRService from './VRService/VRService';
 
 const DetailService = () => {
   return (
     <>
-      <PageTitle variant='detail-service-page'/>
+      <ScrollToTop />
+      <PageTitle variant="detail-service-page" />
       <VRService />
-      <GetStarted variant='detail-service-page'/>
-      <Pricing  variant='detail-service-page'/>
+      <GetStarted variant="detail-service-page" />
+      <Pricing variant="detail-service-page" />
       <Subscribe />
     </>
   );
