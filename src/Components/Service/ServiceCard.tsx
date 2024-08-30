55f2f21d1d07af05cf62186b0bf6d0df69096fef
SolisTenebrum <solistenebrum@icloud.com>
2024-08-30 18:11:03 +0500

    added mobile version for service cards

--- src/Components/Service/ServiceCard.tsx
+++ src/Components/Service/ServiceCard.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Service.module.css';
+import { clsx as cn } from 'clsx';
+import isMobileDevice from '../../utils/isMobileDevice';
 
 const ServiceCard = ({
   icon,
@@ -14,13 +16,15 @@ const ServiceCard = ({
 }): JSX.Element => {
   return (
     <>
-      <li className={styles.card}>
-          <img src={icon} className={styles.icon} />
-          <h3 className={styles.cardTitle}>{title}</h3>
-          <p className={styles.cardText}>{text}</p>
-          <NavLink to={`/detail-service?serviceTitle=${title}#vrservice`}>
-            <button className={styles.button}>{buttonText}</button>
-          </NavLink>
+      <li className={cn(styles.card, isMobileDevice() === 'mobile' ? styles.card_mobile : '')}>
+        <img src={icon} className={styles.icon} />
+        <h3 className={styles.cardTitle}>{title}</h3>
+        <p className={styles.cardText}>{text}</p>
+        <NavLink to={`/detail-service?serviceTitle=${title}#vrservice`} className={styles.navlink}>
+          <button className={cn(styles.button, isMobileDevice() === 'mobile' ? styles.button_mobile : '')}>
+            {buttonText}
+          </button>
+        </NavLink>
       </li>
     </>
   );
